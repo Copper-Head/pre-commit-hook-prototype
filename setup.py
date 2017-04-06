1422e33496598cf647b71a42c97cd09cdc62e69a
Ilia Kurenkov <ilia.kurenkov@egym.de>
2017-04-06 17:41:11 +0200

    more documentation for setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+"""This file makes our hooks easy to install by pre-commit.
+Python packages seem to be the dominating way hooks can be installed as.
+There's also support for node.js and ruby packages as well as docker images.
+From what I understood there was no support for something like make.
+
+See docs here:
+http://pre-commit.com/#usage
+And here:
+http://pre-commit.com/#new-hooks
+"""
 from setuptools import setup
 
 
 setup(
+    # any random silly name for our dummy python package.
     name='pre_commit_hook_prototype',
     version='0.0.0',
     # This installs the dependencies
