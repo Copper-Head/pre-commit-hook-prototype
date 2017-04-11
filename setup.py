bdb47c160e4498a671a25cf11ad2fd90cfb9cf9e
Ilia Kurenkov <ilia.kurenkov@egym.de>
2017-04-11 12:41:49 +0200

    First take on a setup that should work

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     # This installs the dependencies
     install_requires=['yapf==0.16.1', 'docformatter==0.8'],
     # This places our yapf.conf in the right place
-    data_files=[('/etc/prototype', ['example_yapf.conf'])]
+    data_files=[('$HOME', ['egym_yapf.conf'])]
 )
